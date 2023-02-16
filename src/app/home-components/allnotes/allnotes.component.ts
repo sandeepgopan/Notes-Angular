83f2a97e6bdbd3909ea2d2fd58dc071d59f2d3c2
richujain <richujain1995@gmail.com>
2023-02-16 02:58:45 -0500

    Added menu buttons and designed AllNotes component

--- src/app/home-components/allnotes/allnotes.component.ts
+++ src/app/home-components/allnotes/allnotes.component.ts
@@ -13,20 +13,50 @@ import tinymce from 'tinymce';
 })
 export class AllnotesComponent implements OnInit {
   uid: any = '';
+  // data: Array<{
+  //   title: string;
+  //   scribble: string;
+  //   category: string;
+  //   owner: string;
+  // }> = [];
+
+  data: Array<any> = [];
+  single: any = '';
+  scribbles: Array<any> = [];
 
   constructor(
     private router: Router,
     // private firestore: AngularFirestore,
     // private auth: AuthService,
-    private firebaseAuth: Auth
+    private firebaseAuth: Auth,
+    private firestore: AngularFirestore
   ) {
     authState(this.firebaseAuth).subscribe((response) => {
-      console.log(response);
+      // console.log(response);
       this.uid = response?.uid;
     });
   }
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.loadScribblesFromDatabase();
+  }
   newScribble() {
     this.router.navigate(['/scribble']);
+  }
+  loadScribblesFromDatabase() {
+    this.firestore
+      .collection('users')
+      .get()
+      .subscribe((ss) => {
+        ss.docs.forEach((doc) => {
+          this.data.push(doc.data());
+          this.single = this.data[3];
+          // console.log(Object.keys(this.data));
+          // {owner: 'IiqXhN4lMcc8tXkx7DqjOZQFsuD3', scribble: '<h3 style="text-align: center;">This is a sample</…gn: center;">Hi there, this is a sample test.</p>', title: 'Sample Title Name', category: 'All'}
+        });
+        this.data.forEach((d) => {
+          this.scribbles.push(d.scribble);
+        });
+        console.log(this.data);
+      });
   }
 }
